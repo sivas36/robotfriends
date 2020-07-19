59d2a076da1a0e869012ced779ca0ba60da0707e
sivakumar <67056444+sivas36@users.noreply.github.com>
2020-07-19 15:42:15 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Siva',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'rilwan',
+    username: 'Antonette',
+    email: 'rillu@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'vengadanathan',
+    username: 'Samantha',
+    email: 'elumalaiiyarponnu.net'
+  },
+  {
+    id: 4,
+    name: 'moni',
+    username: 'ghjdbUI',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Meenakshi',
+    username: 'Kamren',
+    email: 'black_love@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'ramya',
+    username: 'Leopoldo_Corkery',
+    email: 'ramya@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'jeisurya',
+    username: 'Elwyn.Skiles',
+    email: 'jei@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'parthiban',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'selciya',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'priyanka',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
